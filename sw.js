2e044dc6395f8a8d2c179c215cf6c7f752908435
Awakened <marcandre.lecompte9@gmail.com>
2026-07-11 22:56:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Enables full offline support and PWA installation
 // Strategy: network-first for code files (HTML/JS/CSS), cache-first for assets (images/fonts)
 
-const CACHE_NAME = 'awakened-v544';
+const CACHE_NAME = 'awakened-v545';
 const ASSETS = [
   '/',
   '/index.html',
